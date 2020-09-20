43313a32406631f183849b96d741dd9420058329
Ethan Qiu HOME <balancein0and1@gmail.com>
2020-09-19 20:03:57 -0400

    L

--- ImgEdit.py
+++ ImgEdit.py
@@ -1,26 +1,33 @@
-import cv2
 import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+import cv2
+import time
+import os
 
-# white blank image
-blank_image2 = 255 * np.ones(shape=[512, 1024, 3], dtype=np.uint8)
-# font
-font = cv2.FONT_HERSHEY_PLAIN
+## Make canvas and set the color
+img = np.zeros((200,1000,3),np.uint8)
+b,g,r,a = 255,255,255,0
 
-# org
-org = (0, 200)
+## Use cv2.FONT_HERSHEY_XXX to write English.
+text = time.strftime("%Y/%m/%d %H:%M:%S %Z", time.localtime())
+cv2.putText(img,  "text", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (b,g,r))
 
-# fontScale
-fontScale = 2
 
-# Blue color in BGR
-color = (0, 0, 0)
+## Use simsum.ttc to write Chinese.
+def get_img(name):
+    my_path = os.path.dirname(__file__)
+    fontpath = my_path + "/fonts/English.ttf" # <== 这里是宋体路径
+    img1 = np.zeros((200, 1000, 3), np.uint8)
+    font = ImageFont.truetype(fontpath, 32)
 
-# Line thickness of 2 px
-thickness = 2
+    img_pil = Image.fromarray(img)
+    draw = ImageDraw.Draw(img_pil)
+    draw.text((50, 80),  "مرحبا", font = font, fill = (b, g, r))
+    img1 = np.array(img_pil)
+    return img1
 
-# Using cv2.putText() method
-image = cv2.putText(blank_image2, '检体', org, font,
-                    fontScale, color, thickness, cv2.LINE_AA)
-cv2.imshow("White Blank", blank_image2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+img = get_img("f")
+## Display
+cv2.imshow("res", img);cv2.waitKey();cv2.destroyAllWindows()
+#cv2.imwrite("res.png", img)
