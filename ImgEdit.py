58536e9fc3f76c58e620f630f8b1d856ae5dc32b
Tianshuang(Ethan) Qiu <41294301+TianshuangQiu@users.noreply.github.com>
2020-09-21 17:52:09 -0400

    In class rush

--- ImgEdit.py
+++ ImgEdit.py
@@ -10,24 +10,27 @@ b,g,r,a = 255,255,255,0
 
 ## Use cv2.FONT_HERSHEY_XXX to write English.
 text = time.strftime("%Y/%m/%d %H:%M:%S %Z", time.localtime())
-cv2.putText(img,  "text", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (b,g,r))
+cv2.putText(img,  "This is a statement", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (b,g,r))
 
 
 ## Use simsum.ttc to write Chinese.
 def get_img(name):
     my_path = os.path.dirname(__file__)
     fontpath = my_path + "/fonts/English.ttf" # <== 这里是宋体路径
-    img1 = np.zeros((200, 1000, 3), np.uint8)
+    img1 = np.zeros((500, 700, 3), np.uint8)
     font = ImageFont.truetype(fontpath, 32)
 
     img_pil = Image.fromarray(img)
     draw = ImageDraw.Draw(img_pil)
-    draw.text((50, 80),  "مرحبا", font = font, fill = (b, g, r))
+    draw.text((50, 80),  "这是一句话", font = font, fill = (b, g, r))
     img1 = np.array(img_pil)
     return img1
 
 
 img = get_img("f")
+img = cv2.circle(img, (120, 50), 50, (0, 0, 0), -1)
+img = cv2.circle(img, (300, 500), 50, (0, 0, 0), -1)
+img = cv2.circle(img, (400, 50), 50, (0, 0, 0), -1)
+
 ## Display
-cv2.imshow("res", img);cv2.waitKey();cv2.destroyAllWindows()
-#cv2.imwrite("res.png", img)
+cv2.imwrite("res.png", img)
