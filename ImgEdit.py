ec792a25f3a44b2bd3111c3ffafbc3e651408341
Tianshuang(Ethan) Qiu <41294301+TianshuangQiu@users.noreply.github.com>
2020-10-24 13:40:02 -0400

    cmt

--- ImgEdit.py
+++ ImgEdit.py
@@ -18,7 +18,7 @@ def get_img(name):
     my_path = os.path.dirname(__file__)
     fontpath = my_path + "/fonts/English.ttf" # <== 这里是宋体路径
     img1 = np.zeros((500, 700, 3), np.uint8)
-    font = ImageFont.truetype(fontpath, 32)
+    font = ImageFont.truetype(fontpath, 50)
 
     img_pil = Image.fromarray(img)
     draw = ImageDraw.Draw(img_pil)
@@ -28,9 +28,9 @@ def get_img(name):
 
 
 img = get_img("f")
-img = cv2.circle(img, (120, 50), 50, (0, 0, 0), -1)
-img = cv2.circle(img, (300, 500), 50, (0, 0, 0), -1)
-img = cv2.circle(img, (400, 50), 50, (0, 0, 0), -1)
+img = cv2.circle(img, (120, 50), 20, (255, 255, 255), -1)
+img = cv2.circle(img, (350, 20), 50, (255, 255, 255), -1)
+img = cv2.circle(img, (100, 90), 30, (255, 255, 255), -1)
 
 ## Display
 cv2.imwrite("res.png", img)
