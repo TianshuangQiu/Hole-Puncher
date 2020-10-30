e7d51526d53ada750ec126e6c53632e006c5d518
Tianshuang(Ethan) Qiu <41294301+TianshuangQiu@users.noreply.github.com>
2020-10-29 22:14:29 -0400

    Oct 29 1

--- ImgEdit.py
+++ ImgEdit.py
@@ -1,36 +1,44 @@
 import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 import cv2
-import time
+import random
 import os
 
-## Make canvas and set the color
-img = np.zeros((200,1000,3),np.uint8)
-b,g,r,a = 255,255,255,0
+# Make canvas and set the color
+height = 100
+width = 1000
+img = np.zeros((height, width, 3), np.uint8)
+b, g, r, a = 255, 255, 255, 0
 
-## Use cv2.FONT_HERSHEY_XXX to write English.
-text = time.strftime("%Y/%m/%d %H:%M:%S %Z", time.localtime())
-cv2.putText(img,  "This is a statement", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (b,g,r))
+# Use cv2.FONT_HERSHEY_XXX to write English.
 
 
-## Use simsum.ttc to write Chinese.
-def get_img(name):
+def get_img():
     my_path = os.path.dirname(__file__)
-    fontpath = my_path + "/fonts/English.ttf" # <== 这里是宋体路径
-    img1 = np.zeros((500, 700, 3), np.uint8)
-    font = ImageFont.truetype(fontpath, 50)
+    fontpath = my_path + "/fonts/English.ttf"  # <== 这里是宋体路径
+    img1 = np.zeros((height, width, 3), np.uint8)
+    font = ImageFont.truetype(fontpath, 70)
 
     img_pil = Image.fromarray(img)
     draw = ImageDraw.Draw(img_pil)
-    draw.text((50, 80),  "这是一句话", font = font, fill = (b, g, r))
+    draw.text((20, 25),  "你听懂了吗", stroke_height=1, font=font, fill=(b, g, r))
     img1 = np.array(img_pil)
     return img1
 
 
-img = get_img("f")
-img = cv2.circle(img, (120, 50), 20, (255, 255, 255), -1)
-img = cv2.circle(img, (350, 20), 50, (255, 255, 255), -1)
-img = cv2.circle(img, (100, 90), 30, (255, 255, 255), -1)
+img = get_img()
 
-## Display
+count = 0
+for x in range (0,height):
+    for y in range (0,width):
+        curr_val = img[x][y]
+        rand = random.randint(0,100)
+        if rand<20:
+            img[x][y] = [255,255,255]
+            count+=1
+        if rand>80:
+            img[x][y] = [0, 0, 0]
+            count += 1
+
+# Display
 cv2.imwrite("res.png", img)
